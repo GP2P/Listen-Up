230589e179e7f3c4c9e5ed195819114dfa0b1ffb
G3P <73323107+GP2P@users.noreply.github.com>
2023-10-27 23:40:04 -0700

    Initial Commit

---
+++ Listen Up/Listen_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Listen_UpApp.swift
+//  Listen Up
+//
+//  Created by G3P on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Listen_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
